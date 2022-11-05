2991b05516a0f7a9c140f4796a5714b78758288f
ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠ <29128703+milksense@users.noreply.github.com>
2022-11-05 14:34:44 +0000

    :v:

---
+++ README.md
@@ -0,0 +1,30 @@
+## Material You baseline color scheme tokens
+
+The M3 baseline color scheme uses the same roles and token mapping as user-generated color schemes. For full details on working with color roles, see the applying color schemes section. Jump to the tokens table [tokens].
+
+#### Light theme
+
+![](https://lh3.googleusercontent.com/KvS4VAUMcFwCX3FDFOzuAGoO6Okpk5JXhnCkzRx9ehxCML6-FZ_cdJw20-mDbifLYrbTlULpM_SvIhl9n_T9zjQtT78MDThbxeEvIb1brKq0=s0)
+
+#### Dark theme
+
+![](https://lh3.googleusercontent.com/nQHmWgLpXxjfV9nC_xIabgJDagi5V3aBB9qbFRA_EHEkEeTaq3uh-rYwoXnkRqL1eHCobVjb8lmQgdistb_XNcCfVdsQqUC-h0hvje4j6Qk=s0)
+
+## Using
+
+```dart
+import 'package:example/palette.dart';
+// ...
+final Color m3BaseColor = Palette.primary[80]!;
+```
+
+![](https://lh3.googleusercontent.com/lDy0hwp27tHvZ2dfXiAou3KWtypk-KibE0e1Vj9ofmcMFjgIXV-0tYSE33cRKvyflp0qOH0FTicJaH8QLj92Vn7p_doqBwURJxBNaDPzBGQ=s0)
+
+```dart
+import 'package:example/palette.dart';
+// ...
+// Dark theme, background
+final Color m3BaseColor = Palette.neutral[10]!;
+// Light theme, background
+final Color m3BaseColor = Palette.primary[99]!;
+```
---
+++ palette.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const MaterialColor primary = MaterialColor(
+    0xff000000,
+    <int, Color>{
+      10: Color(0xff21005d),
+      20: Color(0xff381e72),
+      30: Color(0xff4f378b),
+      40: Color(0xff6750a4),
+      50: Color(0xff7f67be),
+      60: Color(0xff9a82db),
+      70: Color(0xffb69df8),
+      80: Color(0xffd0bcff),
+      90: Color(0xffeaddff),
+      95: Color(0xfff6edff),
+      99: Color(0xfffffbfe),
+      100: Color(0xffffffff),
+    },
+  );
+
+  static const MaterialColor secondary = MaterialColor(
+    0xff000000,
+    <int, Color>{
+      10: Color(0xff1d192b),
+      20: Color(0xff332d41),
+      30: Color(0xff4a4458),
+      40: Color(0xff625b71),
+      50: Color(0xff7a7289),
+      60: Color(0xff958da5),
+      70: Color(0xffb0a7c0),
+      80: Color(0xffccc2dc),
+      90: Color(0xffe8def8),
+      95: Color(0xfff6edff),
+      99: Color(0xfffffbfe),
+      100: Color(0xffffffff),
+    },
+  );
+
+  static const MaterialColor tertiary = MaterialColor(
+    0xff000000,
+    <int, Color>{
+      10: Color(0xff31111d),
+      20: Color(0xff492532),
+      30: Color(0xff633b48),
+      40: Color(0xff7d5260),
+      50: Color(0xff986977),
+      60: Color(0xffb58392),
+      70: Color(0xffd29dac),
+      80: Color(0xffefb8c8),
+      90: Color(0xffffd8e4),
+      95: Color(0xffffecf1),
+      99: Color(0xfffffbfa),
+      100: Color(0xffffffff),
+    },
+  );
+
+  static const MaterialColor error = MaterialColor(
+    0xff000000,
+    <int, Color>{
+      10: Color(0xff410e0b),
+      20: Color(0xff601410),
+      30: Color(0xff8c1d18),
+      40: Color(0xffb3261e),
+      50: Color(0xffdc362e),
+      60: Color(0xffe46962),
+      70: Color(0xffec928e),
+      80: Color(0xfff2b8b5),
+      90: Color(0xfff9dedc),
+      95: Color(0xfffceeee),
+      99: Color(0xfffffbf9),
+      100: Color(0xffffffff),
+    },
+  );
+
+  static const MaterialColor neutral = MaterialColor(
+    0xff000000,
+    <int, Color>{
+      10: Color(0xff1c1b1f),
+      20: Color(0xff313033),
+      30: Color(0xff484649),
+      40: Color(0xff605d62),
+      50: Color(0xff787579),
+      60: Color(0xff939094),
+      70: Color(0xffaeaaae),
+      80: Color(0xffc9c5ca),
+      90: Color(0xffe6e1e5),
+      95: Color(0xfff4eff4),
+      99: Color(0xfffffbfe),
+      100: Color(0xffffffff),
+    },
+  );
+
+  static const MaterialColor neutral_variant = MaterialColor(
+    0xff000000,
+    <int, Color>{
+      10: Color(0xff1d1a22),
+      20: Color(0xff322f37),
+      30: Color(0xff49454f),
+      40: Color(0xff605d66),
+      50: Color(0xff79747e),
+      60: Color(0xff938f99),
+      70: Color(0xffaea9b4),
+      80: Color(0xffcac4d0),
+      90: Color(0xffe7e0ec),
+      95: Color(0xfff5eefa),
+      99: Color(0xfffffbfe),
+      100: Color(0xffffffff),
+    },
+  );
+}
